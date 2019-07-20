62857ca70dcfd000c594de7613df0a8b807dfa0a
Алексей Ларюшкин <alexey.lariushkin@rtlabs.ru>
2019-07-20 20:45:33 +0400

    some fixes 2

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -42,7 +42,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         NORMAL(Triple(255, 255, 255)),
         WARNING(Triple(255, 120, 0)),
         DANGER(Triple(255, 60, 60)),
-        CRITICAL(Triple(255, 255, 0));
+        CRITICAL(Triple(255, 0, 0));
 
         fun nextStatus(): Status {
             return if (this.ordinal < values().lastIndex) {
@@ -56,19 +56,19 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     enum class Question(val question: String, val answers: List<String>) {
         NAME("Как меня зовут?", listOf("бендер", "bender")) {
             override fun nextQuestion(): Question = PROFESSION
-            override fun validateAnswer(answer: String): Boolean = answer[0].isUpperCase()
+            override fun validateAnswer(answer: String): Boolean = answer.firstOrNull()?.isUpperCase() ?: false
         },
         PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
             override fun nextQuestion(): Question = MATERIAL
-            override fun validateAnswer(answer: String): Boolean = answer[0].isLowerCase()
+            override fun validateAnswer(answer: String): Boolean = answer.firstOrNull()?.isLowerCase() ?: false
         },
         MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
             override fun nextQuestion(): Question = BDAY
-            override fun validateAnswer(answer: String): Boolean = answer.all { !it.isDigit() }
+            override fun validateAnswer(answer: String): Boolean = answer.isNotEmpty() && answer.all { !it.isDigit() }
         },
         BDAY("Когда меня создали?", listOf("2993")) {
             override fun nextQuestion(): Question = SERIAL
-            override fun validateAnswer(answer: String): Boolean = answer.all { it.isDigit() }
+            override fun validateAnswer(answer: String): Boolean = answer.isNotEmpty() && answer.all { it.isDigit() }
         },
         SERIAL("Мой серийный номер?", listOf("2716057")) {
             override fun nextQuestion(): Question = IDLE
